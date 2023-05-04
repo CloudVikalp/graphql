b065b57b295be1d7300413556b3c3379bbd5243f
CloudVikalp <vikalp.shandilya@cloudactivelabs.in>
2023-05-04 18:28:17 +0530

    client part

---
+++ Blog/client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Profile from './components/Profile';
+import CreateQuote from './components/CreateQuote';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div>
+      {/*
+      <Login />
+      <Signup />
+      <Profile />
+      <CreateQuote /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
